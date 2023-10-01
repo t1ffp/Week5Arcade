ede421e2d4fcb04b4ab864295c9a0b5426dd9bcc
t1ffp <pengt796@newschool.edu>
2023-10-01 07:05:49 -0400

    commit

---
+++ Arcade/Assets/Scripts/CopyLimb.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyLimb : MonoBehaviour
+{
+    [SerializeField] private Transform targetLimb;
+    private ConfigurableJoint m_ConfigurableJoint;
+
+    Quaternion targetInitialRotation;
+
+    void Start()
+    {
+        this.m_ConfigurableJoint = this.GetComponent<ConfigurableJoint>();
+        this.targetInitialRotation = this.targetLimb.transform.localRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        this.m_ConfigurableJoint.targetRotation = CopyRotation();
+    }
+
+    private Quaternion CopyRotation()
+    {
+        return Quaternion.Inverse(this.targetLimb.localRotation) * this.targetInitialRotation;
+    }
+}
---
+++ Arcade/Assets/Scripts/OtherCharacterController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class OtherCharacterController : MonoBehaviour
+{
+    private static Vector2 _input;
+    private CharacterController _characterController;
+    private static Vector3 _direction;
+
+    
+}
